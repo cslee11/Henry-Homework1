c907a44e0a1f96d8504d6bcc6e5207a56cd99854
cslee11 <75759362+cslee11@users.noreply.github.com>
2021-03-24 03:06:52 -0500

    did firs half of hw

---
+++ main.js
@@ -0,0 +1,36 @@
+
+// Area = pi(radius)^2  ---> pi = Math.PI
+// radius will be given as an argument
+// Math.pow(base, exponent)
+// num.toFixed(number of decimal places)
+
+
+// ---First assignment with BONUS ---
+// function areaOfCircle(radius) {
+//     return areaOfCircle = ('The area for a circle with a radius of ' + radius + ' is ' + (Math.pow(radius,2) * Math.PI).toFixed(2) + '.');
+// }
+// areaOfCircle(2);
+// console.log(areaOfCircle);      //logs: The area for a cicle with a radius of 2 is 12.57.
+
+
+
+
+//---Write a function that will take one argument (a number) and perform the following operations, using the functions you wrote earlier1:
+
+function areaOfCircle(radius){
+    return area = (Math.pow(radius,2) * Math.PI).toFixed(2);
+}
+
+// 1.) Take half of the number and store the result.
+function half(){
+    let number = areaOfCircle(2)/2;
+    return number   
+}
+
+half();
+console.log(number);
+
+
+// 2.) Square the result of #1 and store that result.
+// 3.) Calculate the area of a circle with the result of #2 as the radius.
+// 4.) Calculate what percentage that area is of the squared result (#3).
