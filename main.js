7c927f49f168ceba3d95492cb0f4c412bc00db4b
cslee11 <75759362+cslee11@users.noreply.github.com>
2021-03-24 17:53:40 -0500

    new

--- main.js
+++ main.js
@@ -5,6 +5,8 @@
 // num.toFixed(number of decimal places)
 
 
+// let area = (radius(Math.pow(radius,2)* Math.PI).toFixed(2);
+
 // ---First assignment with BONUS ---
 // function areaOfCircle(radius) {
 //     return areaOfCircle = ('The area for a circle with a radius of ' + radius + ' is ' + (Math.pow(radius,2) * Math.PI).toFixed(2) + '.');
@@ -12,25 +14,46 @@
 // areaOfCircle(2);
 // console.log(areaOfCircle);      //logs: The area for a cicle with a radius of 2 is 12.57.
 
+// function areaOfCircle(radius) {
+//     area
+//     return areaOfCircle;
+// }
+
+
+
 
 
 
 //---Write a function that will take one argument (a number) and perform the following operations, using the functions you wrote earlier1:
 
-function areaOfCircle(radius){
-    return area = (Math.pow(radius,2) * Math.PI).toFixed(2);
-}
+// function areaOfCircle(radius){
+//     return area = (Math.pow(radius,2) * Math.PI).toFixed(2);
+// }
 
-// 1.) Take half of the number and store the result.
-function half(){
-    let number = areaOfCircle(2)/2;
-    return number   
-}
+// // 1.) Take half of the number and store the result.
 
-half();
-console.log(number);
 
+
+// function half(num) {
+//     let x = num/2;
+//     console.log(x);
+//     return (x);
+// // }
+// half(8);
 
 // 2.) Square the result of #1 and store that result.
+// function square(num) {
+//     let y = Math.pow(num,2);
+//     console.log(y);
+// }
+// square(4);
+
+
 // 3.) Calculate the area of a circle with the result of #2 as the radius.
+function areaOfCircle(radius) {
+        return areaOfCircle = (radius(Math.pow(radius,2) * Math.PI).toFixed(2));
+};
+
+areaOfCircle(16);
+console.log(areaOfCircle);
 // 4.) Calculate what percentage that area is of the squared result (#3).
